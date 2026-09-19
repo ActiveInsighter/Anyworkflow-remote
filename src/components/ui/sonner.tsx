b11668d632e15a7b8a4ca4283d155dfe42c5b363
Li Bowei <149855691+ActiveInsighter@users.noreply.github.com>
2026-09-19 20:08:44 +0800

    fix: restore Sonner default UI

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,5 @@
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
 
 export function Toaster(props: ToasterProps) {
-  return (
-    <Sonner
-      position="top-center"
-      duration={2400}
-      visibleToasts={1}
-      expand={false}
-      swipeDirections={['top', 'left', 'right']}
-      {...props}
-    />
-  )
+  return <Sonner {...props} />
 }
